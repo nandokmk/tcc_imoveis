1254e2dd8b6ad62263c91492d1b645c0df425bfd
unknown <Petto@.(none)>
2011-03-23 15:51:13 -0300

    inicializando repositorio com asp.net mvc e teste google maps test

---
+++ Tcc_Imoveis/Controllers/MapController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tcc_Imoveis.Controllers
+{
+    public class MapController : Controller
+    {
+        //
+        // GET: /Map/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
