1545b1a980900d0e3554b164b5622082fe8c9868
unknown <Petto@.(none)>
2011-05-09 15:54:16 -0300

    Insercao do layout do admin

---
+++ Tcc_Imoveis/Controllers/ImovelController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tcc_Imoveis.Models;
+
+namespace Tcc_Imoveis.Controllers
+{
+    public class ImovelController : Controller
+    {
+        //
+        // GET: /Imovel/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Imovel/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Imovel/Create
+
+        public ActionResult Create()
+        {
+            
+            using (var db = new tcc_imoveisEntities1())
+            {
+                List<bairro> bairros = db.bairro.ToList();
+                ViewBag.bairros = bairros;
+            }
+
+
+            return View();
+        } 
+
+        //
+        // POST: /Imovel/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        //
+        // GET: /Imovel/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Imovel/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Imovel/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Imovel/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Tcc_Imoveis/Controllers/SearchController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tcc_Imoveis.Models;
+
+namespace Tcc_Imoveis.Controllers
+{
+    public class SearchController : Controller
+    {
+        //
+        // GET: /Search/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Fernando()
+        {
+            
+
+            ViewBag.Fernando = "Programador Analista";
+            return View();
+        }
+
+
+    }
+}
